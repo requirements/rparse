8f1d5f4cc0226dacf7ca3642968bc6f6ab91ace5
Dmitry Veselov <d.a.veselov@yandex.ru>
2015-02-04 04:03:48 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,22 +6,21 @@ class RParseTestCase(unittest.TestCase):
 
     def test_parse_loosy_requirement(self):
         requirements = "flask"
-        ast = next(rparse.parse(requirements))
-        self.assertEqual(list(ast.select("package > name *")), ["flask"])
+        package, version = next(rparse.parse(requirements))
+        self.assertEqual(package, "flask")
+        self.assertEqual(version, None)
 
     def test_parse_strict_requirement(self):
         requirements = "flask==0.10.1"
-        ast = next(rparse.parse(requirements))
-        self.assertEqual(list(ast.select("package > name *")), ["flask"])
-        self.assertEqual(list(ast.select("package > vspec > comparison *")), ["=="])
-        self.assertEqual(list(ast.select("package > vspec > version *")), ["0.10.1"])
+        package, version = next(rparse.parse(requirements))
+        self.assertEqual(package, "flask")
+        self.assertEqual(version, [('==', '0.10.1')])
 
     def test_parse_multiple_versions(self):
         requirements = "flask>=0.10.1, <0.11"
-        ast = next(rparse.parse(requirements))
-        self.assertEqual(list(ast.select("package > name *")), ["flask"])
-        self.assertEqual(list(ast.select("package > vspec > comparison *")), [">=", "<"])
-        self.assertEqual(list(ast.select("package > vspec > version *")), ["0.10.1", "0.11"])
+        package, version = next(rparse.parse(requirements))
+        self.assertEqual(package, "flask")
+        self.assertEqual(version, [('>=', '0.10.1'), ('<', '0.11')])
 
     def test_parse_invalid_requirements(self):
         requirements = """
@@ -31,7 +30,6 @@ class RParseTestCase(unittest.TestCase):
         ast = rparse.parse(requirements)
         with self.assertRaises(ValueError, message="Invalid requirements line: 'flask 0.10.1'"):
             next(ast)
-        ast = next(ast)
-        self.assertEqual(list(ast.select("package > name *")), ["redis"])
-        self.assertEqual(list(ast.select("package > vspec > comparison *")), ["=="])
-        self.assertEqual(list(ast.select("package > vspec > version *")), ["1.0"])
+        package, version = next(ast)
+        self.assertEqual(package, "redis")
+        self.assertEqual(version, [('==', '1.0')])
